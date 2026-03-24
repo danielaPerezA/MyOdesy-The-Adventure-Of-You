0cfac29f54c5ba58c09066ecef3a01d3cbf97df6
Celene <celeneparra12@gmail.com>
2026-03-24 18:40:33 -0500

    Se crea el proyecto y se realiza el login/register

---
+++ MyOdesy/src/app/app.routes.ts
@@ -0,0 +1,12 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+
+  {
+    path: 'login',
+    loadComponent: () => import('./pages/login-register/login-register')
+      .then(m => m.LoginRegister)
+  }
+
+];
